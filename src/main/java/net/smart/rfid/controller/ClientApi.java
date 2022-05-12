d8da886a40ce0983ed47314abf5b6e769a8b973d
Gabriele <ruggzan@gmail.com>
2022-05-12 12:25:52 +0200

    aa

--- src/main/java/net/smart/rfid/controller/ClientApi.java
+++ src/main/java/net/smart/rfid/controller/ClientApi.java
@@ -2,8 +2,8 @@ package net.smart.rfid.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +25,7 @@ import net.smart.rfid.response.ClientRespSP;
 @RequestMapping("")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ClientApi {
-	Logger logger = Logger.getLogger(ClientApi.class);
+	Logger logger = Logger.getLogger("ClientApi");
 
 	@Autowired
 	private ClientRepository clientRepository;
